d36ad4054675dfffdadda6cf8d6afffe6d90bfb0
kazz <gang9696@gmail.com>
2023-12-27 23:51:27 +0900

    FRotation

--- SecondUEgame.cpp
+++ SecondUEgame.cpp
@@ -2,26 +2,34 @@
 /*
 void AMovingPlatform::MovePlatform(float DeltaTime)
 {
-	FVector CurrentLocation = GetActorLocation();
-	CurrentLocation += (PlatformVelocity * DeltaTime);
-	SetActorLocation(CurrentLocation);
-	float distanceMoved = FVector::Dist(StartLocation, CurrentLocation);
-
-	if (distanceMoved > MoveDistance)
+	if(ShouldPlatformReturn())
 	{
-		float OverShoot = distanceMoved - MoveDistance;
-		FString Name = GetName();
-		UE_LOG(LogTemp, Display, TEXT("%s Configured Moved Distance: %f"), *Name, OverShoot);
 		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
 		StartLocation = StartLocation + MoveDirection * MoveDistance; // 100이 되면 다시 되돌아감(-1 하며..)
 		SetActorLocation(StartLocation);
-		PlatformVelocity *= -1;
+		PlatformVelocity = -PlatformVelocity;
+	}
+	else
+	{
+		FVector CurrentLocation = GetActorLocation();	
+		CurrentLocation += (PlatformVelocity * DeltaTime);
+		SetActorLocation(CurrentLocation);
 	}
 }
 
 void AMovingPlatform::RotatePlatform(float DeltaTime)
 {
-	UE_LOG(LogTemp, Display, TEXT("%s Rotating..."), *GetName());
+	AddActorLocalRotation(RotationVelocity * DeltaTime);
+}
+
+bool AMovingPlatform::ShouldPlatformReturn() const
+{
+	return GetDistancedMoved() > MoveDistance;
+}
+
+float AMovingPlatform::GetDistancedMoved() const
+{
+	return FVector::Dist(StartLocation, GetActorLocation());
 }
 
 */
