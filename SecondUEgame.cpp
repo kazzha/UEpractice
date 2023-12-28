03b16a58b15c3f66306b6a8c5b6a73f67475b0c9
kazz <gang9696@gmail.com>
2023-12-28 23:58:13 +0900

    movingPlatform header

--- SecondUEgame.cpp
+++ SecondUEgame.cpp
@@ -1,3 +1,38 @@
+/*
+UCLASS()
+class OBSTACLEASSULT_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(EditAnywhere, Category="Moving")
+	FVector PlatformVelocity = FVector(100, 0, 0);
+	UPROPERTY(EditAnywhere, Category="Moving")
+	float MoveDistance = 100;
+	UPROPERTY(EditAnywhere, Category="Rotation")
+	FRotator RotationVelocity;
+
+	FVector StartLocation;
+
+	void MovePlatform(float DeltaTime);
+	void RotatePlatform(float DeltaTime);
+
+	bool ShouldPlatformReturn() const;
+	float GetDistancedMoved() const;
+};
+*/
 
 /*
 void AMovingPlatform::MovePlatform(float DeltaTime)
